dcea29b510f702edbc0588d96e056798d387e715
1rko <117205339+1rko@users.noreply.github.com>
2024-12-29 18:30:44 +0300

    создана коробка router, функционала еще нет

--- server/index.js
+++ server/index.js
@@ -3,13 +3,22 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
+const router = require('./routes/index')
 
 const app = express()
+app.use(cors())                 //чтоб можно было слать запросы
+app.use(express.json())         //чтоб приложение могло парсить json
+app.use('/api', router)         //привязка корневого роутера
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:'WORKING!!!'})
+})
 
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()      //сверяет с БД схемой
+        await sequelize.authenticate()      //подключение к БД
+        await sequelize.sync()              //сверяет с БД схемой
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     }
     catch (e) {
