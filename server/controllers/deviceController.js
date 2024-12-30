228d8bd3ffcbf180c3be45050e765737c5ef8e62
1rko <117205339+1rko@users.noreply.github.com>
2024-12-30 20:01:58 +0300

    Создан констроллер DeviceController - функция create

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,5 +1,22 @@
+const uuid = require('uuid')
+const path = require('path')         //модуль в node.js
+const {Device} = require('../models/models')
+const ApiError = require('../errors/ApiErrors')
+
 class DeviceController {
-    async create(req, res) {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + '.jpg'       //генерируется уникальное имя файла для картинки
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))    //path.resolve - функция адаптирует путь в ОС, __dirname - путь до текущей папки с контроллерами
+
+            const device = await Device.create({name, price, brandId, typeId, img: fileName})
+
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
 
     }
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
@@ -11,6 +12,7 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const app = express()
 app.use(cors())                 //чтоб можно было слать запросы
 app.use(express.json())         //чтоб приложение могло парсить json
+app.use(fileUpload({}) )         //
 app.use('/api', router)         //привязка корневого роутера
 
 //Обработка ошибок, последний middleware
